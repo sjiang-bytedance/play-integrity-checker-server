b381b43d69a87fd99c318fee0f54c11a577763c8
steven.jiang <steven.jiang@bytedance.com>
2025-08-07 16:11:52 -0700

    base64 key

--- api/check.js
+++ api/check.js
@@ -3,8 +3,9 @@ const playintegrity = google.playintegrity('v1');
 
 
 const packageName = process.env.PACKAGE_NAME
-const privatekey = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
-
+// const privatekey = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+const decoded = Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_B64, 'base64').toString();
+const privatekey = JSON.parse(decoded);
 
 async function getTokenResponse(token) {
 
