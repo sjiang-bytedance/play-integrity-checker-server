1d6369c38ac1b5410d137ececac7a978a11fbf7a
steven.jiang <steven.jiang@bytedance.com>
2025-08-12 15:07:53 -0700

    test

--- api/check.js
+++ api/check.js
@@ -44,6 +44,8 @@ async function getTokenResponse(token) {
   const jwtClient = await getJwtClient();
   google.options({ auth: jwtClient });
 
+  console.log(token);
+
     const res = await playintegrity.v1.decodeIntegrityToken(
         {
             packageName: packageName,
